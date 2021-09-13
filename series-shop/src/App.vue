a4b40c406246d2e4d8f8b73c798f3bd967995034
loudeco <louise.decombe@laplateforme.io>
2021-09-13 13:31:31 +0200

    add header / import components app.vue

--- series-shop/src/App.vue
+++ series-shop/src/App.vue
@@ -1,15 +1,25 @@
 <template>
-<div id="app">
-</div>
+  <div id="app">
+    <the-header></the-header>
+        <div class="d-flex flex-row">
+      // components
+    </div>
+  </div>
+  
 </template>
 
 <script>
+import TheHeader from './components/TheHeader';
+import Shop from './components/Shop/Shop';
+import Cart from './components/Cart/Cart';
+
 export default {
-name: 'app',
-components: {
-}
+  name: 'app',
+  components: {
+    TheHeader,
+    Cart,
+    Shop
+  }
 }
 </script>
 
-<style>
-</style>

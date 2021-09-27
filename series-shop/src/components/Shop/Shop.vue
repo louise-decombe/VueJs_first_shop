28b0b217c5495e46cc50b6da973a3c5ed64d825d
loudeco <louise.decombe@laplateforme.io>
2021-09-27 16:44:10 +0200

    add Shop components content

---
+++ series-shop/src/components/Shop/Shop.vue
@@ -0,0 +1,16 @@
+<template>
+    <shop-products-list class="p-3"></shop-products-list>
+</template>
+
+<script>
+    import ShopProductsList from './ShopProductsList';
+
+    export default {
+        components: {
+            ShopProductsList
+        }
+    }
+</script>
+
+<style>
+</style>
